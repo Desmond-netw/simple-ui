42a004187674d9b783fd6a4e3e3a74915b17dde8
Desmond Deveh <ddeveh.study@gmail.com>
2025-01-26 04:04:35 +0200

    customize storybook config file (main.ts)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -11,6 +14,18 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    config.plugins?.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+      })
+    );
+    return config;
   },
 };
 export default config;
